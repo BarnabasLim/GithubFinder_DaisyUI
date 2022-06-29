9ed92b8a7cb0d5df76ff1c11b5c64e3881d40cdd
BarnabasLim <blim048@e.ntu.edu.sg>
2022-06-29 18:16:22 +0800

    fetch User Github API

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import spinner from '../assets/spinner.gif'
+const Spinner = () => {
+  return (
+    <img src={spinner} alt="Loading" className="spinner" />
+  )
+}
+
+export default Spinner
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import UserResults from '../components/UserResults'
 
 const Home = () => {
   return (
     <div>
         <h1 className="text-6xl">Welcome</h1>
         {process.env.REACT_APP_GITHUB_TOKEN}
+        <UserResults/>
     </div>
   )
 }
